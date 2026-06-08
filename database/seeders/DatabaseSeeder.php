c1d5ff2b5c16263b0217ed2bd94679d88ba18b67
Yonatan Hidson Simbolon <jonatanhudson.s@gmail.com>
2026-06-08 17:40:41 +0700

    Comment out Category and Product seeders
    Commented out CategorySeeder::class and ProductSeeder::class in database/seeders/DatabaseSeeder.php so they are no longer invoked by DatabaseSeeder::run(). This prevents categories and products from being seeded during dev/test runs; uncomment to re-enable.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolePermissionSeeder::class,
-            CategorySeeder::class,
-            ProductSeeder::class
+            // CategorySeeder::class,
+            // ProductSeeder::class
         ]);
 
         $user = User::firstOrCreate(
